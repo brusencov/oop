2bad028eb36aeb013743ddb1050b5a8e7cb0e635
iyasha <mr.robot0n@gmail.com>
2021-03-02 19:37:35 +0200

    v1.1.0 * Программа в продакшне

---
+++ main.py
@@ -0,0 +1,67 @@
+import json
+
+MENU = '1) Добавить\n' \
+       '2) Посмотреть\n' \
+       '3) Выход\n-> '
+
+tasks = []
+
+
+def put_to_db(db_name):
+    global tasks
+    f = open(db_name, 'w')
+    f.write(json.dumps(tasks))
+    f.close()
+
+
+def load_from_db(db_name):
+    try:
+        f = open(db_name)
+    except FileNotFoundError:
+        return []
+    try:
+        tasks = json.loads(f.read())
+        f.close()
+        return tasks
+    except json.JSONDecodeError:
+        return []
+
+
+def add_item_to_db(item):
+    global tasks
+    tasks.append(item)
+
+
+def get_items_from_db():
+    global tasks
+    return ', \n'.join(tasks)
+
+
+def select_menu_item(choose_id):
+    if choose_id == 1:
+        item = input('Enter new item: ')
+        add_item_to_db(item)
+        print(f'{item} added successfully!')
+    elif choose_id == 2:
+        response = get_items_from_db()
+        print(response)
+    elif choose_id == 3:
+        raise Exception('Exit')
+
+
+def main():
+    while True:
+        choose = input(MENU)
+        if choose.isdigit() and int(choose) in [1, 2, 3]:
+            select_menu_item(int(choose))
+        else:
+            print('ERROR')
+
+
+if __name__ == '__main__':
+    db_name = 'data.json'
+    tasks = load_from_db(db_name)
+    try:
+        main()
+    except Exception:
+        put_to_db(db_name)
